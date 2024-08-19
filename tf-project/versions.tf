bcf6574e8e4d26ac0796a02e88a7e8b1e18c8fcf
Adam Domagalski <adam.domagalski@tacton.com>
2024-08-19 12:30:34 +1000

    adjust to acg. play with node tagging (2 ways)

--- tf-project/versions.tf
+++ tf-project/versions.tf
@@ -3,9 +3,6 @@ terraform {
     local = {
       source = "hashicorp/local"
     }
-    openstack = {
-      source = "terraform-provider-openstack/openstack"
-    }
   }
   required_version = ">= 0.13"
 }
